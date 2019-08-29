f7b8a891a964dbc8efd32d03719e7746b2b1aa3c
Niclas Koch <n.koch@medicalvision.de>
2019-08-29 16:31:42 +0200

    gge

--- custom/__init__.py
+++ custom/__init__.py
@@ -30,6 +30,7 @@ def push(message, path, branch):
         colorred = "\033[1;31;40m]"
         colorwhite = "\033[0m 1;31;40m"
         highPath = colorred.format(str(path))
+        print(highPath)
         state = {highPath, colorwhite.format(str(branch)), colorred.format(str(message)), colorwhite.format("")}
         print(state)
     for content in state:
