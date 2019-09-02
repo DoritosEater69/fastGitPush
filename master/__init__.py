2ec873df15328157bb645d7d4122d3be2b77169d
Niclas Koch <n.koch@medicalvision.de>
2019-09-02 11:04:38 +0200

    gwrwgrwgr

--- master/__init__.py
+++ master/__init__.py
@@ -15,33 +15,32 @@ def push(message, path):
         errormsg = error.output, error.returncode, error.message
         print("error", errormsg)
 
-    if "Updates were rejected because a pushed branch tip is behind its remote" in str(errormsg):
-        print("PUSHED BRANCH TIP BEHIND REMOTE")
-        choice = raw_input("Local state will be saved into master-backup Branch! Y/N")
-        if choice == "Y" or choice == "y":
-            os.system("git checkout -b master-backup")
-            os.system("git push origin master-backup")
-            print("Please Merge the States together if needed")
-            time.sleep(3)
-            os.system("git checkout master")
-            os.system("git push origin master")
-            os.system("clear")
+        if "Updates were rejected because a pushed branch tip is behind its remote" in str(errormsg):
+            print("PUSHED BRANCH TIP BEHIND REMOTE")
+            choice = raw_input("Local state will be saved into master-backup Branch! Y/N")
+            if choice == "Y" or choice == "y":
+                os.system("git checkout -b master-backup")
+                os.system("git push origin master-backup")
+                print("Please Merge the States together if needed")
+                time.sleep(3)
+                os.system("git checkout master")
+                os.system("git push origin master")
+                os.system("clear")
+                repoURL = os.system("git config --get remote.origin.url")
+                print(repoURL)
+                quit()
+            if choice == "N" or choice == "n":
+                print("Exiting Program... ")
+                time.sleep(2)
+                quit()
+
+        if "Updates were rejected because the remote contains work that you" in str(errormsg):
             repoURL = os.system("git config --get remote.origin.url")
             print(repoURL)
-            quit()
-        if choice == "N" or choice == "n":
-            print("Exiting Program... ")
-            time.sleep(2)
-            quit()
-    os.system("git push origin master")
-
-    if "Updates were rejected because the remote contains work that you" in str(errormsg):
-        repoURL = os.system("git config --get remote.origin.url")
-        print(repoURL)
-        raw_input("prompt")
-        os.system("git remote add origin repoURL")
-        os.system("git pull origin master")
-        os.system("git push origin master")
+            raw_input("prompt")
+            os.system("git remote add origin repoURL")
+            os.system("git pull origin master")
+            os.system("git push origin master")
 
 
     state = {"Folder: " + path, "Message: " + message, " to Branch: master"}
