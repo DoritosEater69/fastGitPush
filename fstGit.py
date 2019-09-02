033593a1e3d788b84d87876f6e11eb7d436f2bab
Niclas Koch <n.koch@medicalvision.de>
2019-09-02 11:12:44 +0200

    NEW STATE

--- fstGit.py
+++ fstGit.py
@@ -1,18 +1,25 @@
 import os
+import time
 import sys
 import master as mstr
 import custom as cstm
 import subprocess as process
 
-
-path = str(sys.argv[1])
+try:
+    path = str(sys.argv[1])
+except Exception as e:
+    if "list index out of range" in str(e):
+        print("usage: python fstGit.py PATH OF GIT FOLDER")
+        time.sleep(3)
+        os.system("clear")
+        quit()
 
 #master
 def gitMaster(path):
-    mstr.init(path)
+    print(mstr.init(path))
 
 def gitCustom(path):
-    cstm.init(path)
+    print(cstm.init(path))
 
 def menu():
     print("")
